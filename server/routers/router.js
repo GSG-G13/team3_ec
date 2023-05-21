66117ae7b29e75d49d4f65846d1a3fcc870d8a36
KhaledToha <khaled.f.toha@gmail>
2023-05-21 15:41:56 +0200

    error middle ware relates #14 closes #14

--- server/routers/router.js
+++ server/routers/router.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { addUser } from '../controller/users.js';
+import {errHandling} from '../middleware/error.js'
 
 const router = express.Router();
 
-router.post('/signup', addUser);
+router.post('/signup', addUser, errHandling);
 
 router.get('/', (req,res) => {
     res.json({
