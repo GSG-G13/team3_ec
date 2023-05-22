0b87703dfff1bcbbb57bc28372d7c4a180c9e071
Aya Qunoo <qunooa@gmail.com>
2023-05-22 10:30:42 +0200

    1-add get element by id function 2-create components folder 3-create nav and search input field

--- server/controller/getData.js
+++ server/controller/getData.js
@@ -1,5 +1,12 @@
-import { getAllProducts } from '../database/queries/getData.js';
+import { getAllProducts, getProduct } from '../database/queries/getData.js';
 
 export const getProducts = (req, res) => {
   getAllProducts().then((data) => res.json(data.rows));
 };
+export const getProductById = (req, res) => {
+  const { id } = req.params;
+  getProduct(id).then(data => res.json({
+    error: false,
+    data: data.rows,
+  }))
+}
--- server/controller/index.js
+++ server/controller/index.js
@@ -1,8 +1,8 @@
 import addUser from './auth/signup.js';
 import signinController from './auth/signin.js';
 import logOutController from './auth/logout.js';
-import { getProducts } from './getData.js';
+import { getProducts ,getProductById} from './getData.js';
 
 export {
-  addUser, getProducts, logOutController, signinController,
+  addUser, getProducts, logOutController, signinController,getProductById
 };
