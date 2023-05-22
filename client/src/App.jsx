fbc029bfdd625f94761702e02475dd41447c63ce
Yasser Alaa <87650778+YasserAmro0@users.noreply.github.com>
2023-05-22 19:05:41 +0300

    test test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Nav } from './components/Nav/nav';
+import Login from "./components/Login/Login";
 
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Nav/>
-    }
+  },
+  {
+    path: "login",
+    element: <Login/>
+  }
   ])
 
-export default router
+export default router;
