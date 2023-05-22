bb5709f4fa84d4e02fc8ccd2568d5f887bc56a6e
KhaledToha <khaled.f.toha@gmail>
2023-05-22 09:18:18 +0200

    cart backend relates #19 closes #19

---
+++ server/database/queries/cart/addCartItem.js
@@ -0,0 +1,5 @@
+import connection from '../../config/config/connection.js';
+
+export default function addCartItemQuery(user_id, product_id, amount) {
+    return connection.query('INSERT INTO cart (user_id, product_id, amount) VALUES ($1, $2, $3) RETURNING * ;', [user_id, product_id, amount]);
+}
--- server/routers/router.js
+++ server/routers/router.js
@@ -3,11 +3,17 @@ import addUser from '../controller/auth/signup.js';
 import signinController from "../controller/auth/signin.js";
 import logOutController from "../controller/auth/logout.js";
 import { errHandling } from '../middleware/error.js';
+import { getProducts } from '../controller/getProducts.js'
+import authChecker from '../middleware/authChecker.js';
+import { getCartItem } from '../controller/getCartItem.js';
+import addCartItem from '../controller/addCartItem.js';
 
 const router = express.Router();
 
 router.post('/signup', addUser, errHandling);
 router.post('/signin', signinController, errHandling);
 router.use('/logout', logOutController);
-
+router.get('/products', getProducts)
+router.get('/cart', authChecker, getCartItem, errHandling)
+router.post('/cart', authChecker, addCartItem, errHandling)
 export default router;
