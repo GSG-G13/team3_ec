57522d7a32861dca68d566dc58d522dd7982e73c
Yasser Alaa <87650778+YasserAmro0@users.noreply.github.com>
2023-05-22 11:03:34 +0300

    done backend-file

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,9 +11,6 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'import/prefer-default-export': [
-      ('off' || 'warn' || 'error'),
-      { target: 'any' },
-    ],
   },
+  "import/extensions": "off"
 };
