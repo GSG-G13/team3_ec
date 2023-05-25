99d80b3d6f7a46c15ba6a30d1fab951895f998ff
Aya Qunoo <qunooa@gmail.com>
2023-05-25 10:55:20 +0200

    handle errors

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -1,11 +1,11 @@
 export const errHandling = (err, req, res, next) => {
-  
+
   const statusCode = err.status || 501;
   const message = err.message || 'Unknown Error';
 
   res.status(statusCode).json({
     error: true,
-    message,
+    message: message,
   });
   next();
 };
