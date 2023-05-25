8b3081edb1bf5d8ee6aa7ca4955346e7de5b6f39
Aya Qunoo <qunooa@gmail.com>
2023-05-25 12:02:25 +0200

    handle error

--- server/controller/auth/signup.js
+++ server/controller/auth/signup.js
@@ -3,16 +3,21 @@ import { SignUpSchema } from '../../validation/AuthSchema.js';
 import { addUserQuery } from '../../database/queries/auth/signupquery.js';
 import { jwtSign } from '../../helper/jwtSign.js';
 import CustomError from '../../helper/CustomError.js';
+import signInQuery from '../../database/queries/auth/signinquery.js';
 
 const addUser = (req, res, next) => {
-  const { password } = req.body;
+  const { password, email } = req.body;
   let userData;
-  SignUpSchema.validateAsync(req.body, { abortEarly: false })
-    .then(() => bcrybt.hash(password, 7))
-    .then((hashedPassword) => {
-      req.body.password = hashedPassword;
-      return addUserQuery(req.body);
-    })
+  SignUpSchema.validateAsync(req.body, { abortEarly: false }).then(() => signInQuery(email)).then((result) => {
+    if (result.rowCount) {
+      throw new CustomError(400, ["email is already exists!!"])
+    } else {
+      return bcrybt.hash(password, 7)
+    }
+  }).then((hashedPassword) => {
+    req.body.password = hashedPassword;
+    return addUserQuery(req.body);
+  })
     .then((data) => {
       userData = data.rows[0];
       return jwtSign(data.rows[0]);
@@ -29,7 +34,7 @@ const addUser = (req, res, next) => {
         const message = error.details.map((i) => i.message);
         next(new CustomError(400, message));
       } else {
-        next(new CustomError(400, ['this email is already exists']));
+        next(error);
       }
     });
 };
