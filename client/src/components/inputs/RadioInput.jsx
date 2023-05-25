03172abcd14d3e0af818a2989944a68cf23d0190
KhaledToha <khaled.f.toha@gmail>
2023-05-25 03:03:53 +0200

    Pagaination relates #57 closes #57

--- client/src/components/inputs/RadioInput.jsx
+++ client/src/components/inputs/RadioInput.jsx
@@ -3,7 +3,7 @@ import './input.css'
 export const RadioInput = ({label, onChange, value, category}) => {
     return (
         <label className="list-item" >
-        <input type="radio" name="redioBtn" className='radio' onClick={onChange} value={value} checked={value === category} />
+        <input type="radio" name="redioBtn" className='radio' onChange={onChange} value={value} checked={value === category} />
         {label}
         </label>
     )
